120dc9edef1fcb45c2433ab1baf63195fd21416d
NIKHIL SOMAN SAHU <41604652+nikhilsomansahu@users.noreply.github.com>
2024-02-08 15:49:57 +0530

    created ToDO Page

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
+import 'package:hive/hive.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,31 +9,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Box friendbox = Hive.box("friends");
+  String? name;
+  addFriend() async {
+    await friendbox.put("name", "chaman");
+  }
 
+  getFriend() async {
+    setState(() {
+      name = friendbox.get("name");
+    });
+  }
+
+  updateFriend() async {
+    await friendbox.put("name", "gautam gambhir");
+
+  }
+  deleteFriend() async {
+    await friendbox.delete("name");
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Hive DB"),
-        
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: (){}, child: Text("Create")),
-            ElevatedButton(onPressed: (){} , child: Text("Read")),
-            ElevatedButton(onPressed: (){}, child: Text("Update")),
-            ElevatedButton(onPressed: (){}, child: Text("Delete")),
-            GFButton(
-    onPressed: (){},
-    text: "Follow us ",
-   icon: Icon(Icons.facebook),
-   
-  ),
-  
+            Text("$name"),
+            ElevatedButton(onPressed: addFriend, child: Text("Create")),
+            ElevatedButton(onPressed: getFriend, child: Text("Read")),
+            ElevatedButton(onPressed: updateFriend, child: Text("Update")),
+            ElevatedButton(onPressed: deleteFriend, child: Text("Delete")),
           ],
         ),
       ),
